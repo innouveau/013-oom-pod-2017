fc2245d4d9836cbb5c36b4a757991d30efbe16af
Jeroen Breen <jeroen@innouveau.nl>
2017-05-22 12:30:51 +0200

    switch off update option

--- wordpress/2017/wp-content/themes/oom-pod/functions.php
+++ wordpress/2017/wp-content/themes/oom-pod/functions.php
@@ -1,8 +1,8 @@
 <?php
 include('php/meeting-cms.php');
 
-update_option('siteurl', 'http://praktijkopleidersdagen.azurewebsites.net' );
-update_option('home', 'http://praktijkopleidersdagen.azurewebsites.net' );
+//update_option('siteurl', 'http://praktijkopleidersdagen.azurewebsites.net' );
+//update_option('home', 'http://praktijkopleidersdagen.azurewebsites.net' );
 
 
 add_theme_support('post-thumbnails');
